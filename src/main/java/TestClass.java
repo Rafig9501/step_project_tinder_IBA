93535ca92ff7fc3a4cf792c3f1a37af5fb9c1683
Rafig9501 <rafiqmammedzade@gmail.com>
2020-05-12 01:12:46 +0400

    initial commit

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -4,5 +4,6 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("World");
+        System.out.println("hello");
     }
 }
