5471785759afbe0dcf89eb726971586386a0d367
Rafig9501 <rafiqmammedzade@gmail.com>
2020-05-19 03:16:25 +0400

    -- login service written--

--- src/main/java/servlets/LogoutServlet.java
+++ src/main/java/servlets/LogoutServlet.java
@@ -1,5 +1,7 @@
 package servlets;
 
+import utilities.engine.TemplateEngine;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
---
+++ src/main/java/utilities/constants/HttpPaths.java
@@ -0,0 +1,7 @@
+package utilities.constants;
+
+public class HttpPaths {
+
+    public static final String USERS_PAGE = "/users";
+    public static final String LOGIN_PAGE = "/login";
+}
