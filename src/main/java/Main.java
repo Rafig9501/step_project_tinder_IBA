68d8853d91a55eb542343ac6c8bb9663c1455a6b
Shaiq Huseynzade <saiqhuseynzade98@gmail.com>
2020-05-20 14:15:12 +0400

    --updating last_login added --

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,17 @@
-import dao.LikesDao;
 import dao.UserDao;
 import database.JdbcConfig;
 import entity.User;
-import service.UserService;
 
 public class Main {
 
     public static void main(String[] args) {
-        User randomUser = new UserService(new LikesDao(JdbcConfig.getConnection()), new UserDao(JdbcConfig.getConnection())).getRandomUser("25");
-        System.out.println(randomUser);
+//        User randomUser = new UserService(new LikesDao(JdbcConfig.getConnection()), new UserDao(JdbcConfig.getConnection())).getRandomUser("1");
+//        System.out.println(randomUser);
+        User user = new UserDao(JdbcConfig.getConnection()).get("1").get();
+        int lastLogin = new UserDao(JdbcConfig.getConnection()).updateLastLogin("4");
+
+
+        System.out.println(lastLogin);
+
     }
 }
--- src/main/java/servlets/UserServlet.java
+++ src/main/java/servlets/UserServlet.java
@@ -1,5 +1,7 @@
 package servlets;
 
+import entity.User;
+import service.UserService;
 import utilities.engine.TemplateEngine;
 
 import javax.servlet.ServletException;
@@ -12,16 +14,27 @@ import java.io.IOException;
 public class UserServlet extends HttpServlet {
 
     TemplateEngine engine;
+    UserService service;
 
-    public UserServlet(TemplateEngine engine) {
+    public UserServlet(TemplateEngine engine, UserService service) {
         this.engine = engine;
+        this.service = service;
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Cookie[] ck = req.getCookies();
         if (ck != null) {
-            String login = ck[0].getValue();
+//            String login = ck[0].getValue();
+//            String id = ck[1].getValue();
+//            User user = service.getRandomUser(id);
+////            user.getSurname();
+//            String email = user.getEmail();
+//            String photoUrl = user.getPhotoUrl();
+//            String name = user.getName();
+//            String surname = use
+//            System.out.println(user.getEmail());
+//            System.out.println(id);
             engine.render("like-page.ftl", resp);
         } else {
             resp.sendRedirect("/login");
@@ -33,6 +46,7 @@ public class UserServlet extends HttpServlet {
 //        resp.sendRedirect("/users/");
 //        List<User> all = new UserDao(JdbcConfig.getConnection()).getAll();
 //        HashMap<String,Object>
+
         engine.render("like-page.ftl", resp);
 
     }
