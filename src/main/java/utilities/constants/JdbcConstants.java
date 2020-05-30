7dfe6d8dc83bfabcb84dd7f0aab064271e05ce12
Rafig9501 <rafiqmammedzade@gmail.com>
2020-05-30 18:56:35 +0400

    jdbc added

--- src/main/java/utilities/constants/JdbcConstants.java
+++ src/main/java/utilities/constants/JdbcConstants.java
@@ -2,7 +2,7 @@ package utilities.constants;
 
 public class JdbcConstants {
 
-    public static final String URL = System.getenv("DB_URL");
-    public static final String USER_NAME = System.getenv("DB_USER_NAME");
-    public static final String PASSWORD = System.getenv("DB_PASSWORD");
+    public static final String URL = "jdbc:postgresql://ec2-34-230-149-169.compute-1.amazonaws.com:5432/d8f11vb5jqrchi";
+    public static final String USER_NAME = "rjcnpntreuvuoe";
+    public static final String PASSWORD = "d795cabe729ba06336e45954fe9eb50a560ded6df90a17a4a05199a0ce914d59";
 }
